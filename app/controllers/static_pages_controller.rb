38a53400818e5b59d8a2fa5ad76df8455b638b28
KARA <nortonkara057@gmail.com>
2016-04-09 14:16:41 -0400

    add mailers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,14 +4,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.all
   end
-  ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+   ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
   def thank_you
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
-  	ActionMailer::Base.mail(:from => @email,
-  		:to => 'nortonkara057@gmail.com',
-  		:subject => "A new contact form message from #{@name}",
-  		:body => @message).deliver_now
+  	UserMailer.contact_form(@email, @name, @message).deliver_now
+  	#ActionMailer::Base.mail(:from => @email,
+  	#	:to => 'nortonkara057@gmail.com',
+  	#	:subject => "A new contact form message from #{@name}",
+  	#	:body => @message).deliver_now
   end
 end
