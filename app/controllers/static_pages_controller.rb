43c760798ae293e879df0bd780383c8e9af3184e
KARA <nortonkara057@gmail.com>
2016-05-05 08:29:53 -0400

    commented out actionmailer test

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.all
   end
-   ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
+   # ActionMailer::Base.mail(:from => 'from@domain.com', :to => 'to@domain.com', :subject => "Welcome to My Awesome Site", :body => 'I am the email body.').deliver_now
   def thank_you
   	@name = params[:name]
   	@email = params[:email]
