8eb8e96f7296fc7e7ff8dae69fb0a06142c5b01c
KARA <nortonkara057@gmail.com>
2016-06-28 18:06:44 -0400

    fixed variable display

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,40 +1,39 @@
 var app = angular.module('shop', ['ngResource']);
-
-app.factory('models', ['$resource', function($resource){
-  var orders_model = $resource("/orders/:id.json", {id: "@id"});
-  var products_model = $resource("/products/:id.json", {id: "@id"});
-
-  var x = {
-    orders: orders_model,
-    products: products_model
-  };
-
-  return x;
-}]);
-
-$(document).on('ready page:load', function() {
-  angular.bootstrap(document.body, ['shop']);
-});
-
-app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
-  // Here will be all code belonging to this controller
-
-  $scope.orders = models.orders.query();
-  $scope.products = models.products.query();
-
-  $scope.addOrder = function(){
-    if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
-
-    order = models.orders.save($scope.newOrder, function(){
-      recent_order = models.orders.get({id: order.id});
-      $scope.orders.push(recent_order);
-      $scope.newOrder = '';
-    });
-  };
-
-  $scope.deleteOrder = function(order){
-    models.orders.delete(order);
-    
-    $scope.orders.splice($scope.orders.indexOf(order), 1);
-  };
-}]);
+ 
+ $(document).on('ready page:load', function() {
+   angular.bootstrap(document.body, ['shop'])
+ });
+ 
+ app.factory('models', ['$resource', function($resource){
+   var orders_model = $resource("/orders/:id.json", {id: "@id"});
+     var products_model = $resource("/products/:id.json", {id: "@id"});
+     var x = {
+     orders: orders_model,
+     products: products_model
+     };
+    return x;
+  }]);
+ 
+  app.controller('OrdersCtrl', ['$scope', 'models', function($scope, models){
+    //all code here for this controller
+   
+ 
+    $scope.orders = models.orders.query();
+    $scope.products = models.products.query();
+    $scope.addOrder = function(){
+      if(!$scope.newOrder.product_id || $scope.newOrder.total === ''){ return; }
+      // $scope.orders.push($scope.newOrder);
+      order = models.orders.save($scope.newOrder, function(){
+          recent_order = models.orders.get({id: order.id});
+          $scope.orders.push(recent_order);
+          $scope.newOrder = '';
+      });
+    }
+  
+    $scope.deleteOrder = function(order){
+          models.orders.delete(order);
+          $scope.orders.splice($scope.orders.indexOf(order), 1);
+  
+    }
+ 
+ }]);
